b607e219e4b39557b3cf68444c0febff46088c1b
Long Do <longdo@dobtech.vn>
2020-12-28 12:12:05 +0700

    display category meals button with basic info

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -20,7 +21,13 @@ class CategoryMealsScreen extends StatelessWidget {
         title: Text(categoryTitle),
       ),
       body: ListView.builder(
-        itemBuilder: (context, index) => Text(categoryMeals[index].title),
+        itemBuilder: (context, index) => MealItem(
+          imageUrl: categoryMeals[index].imageUrl,
+          title: categoryMeals[index].title,
+          duration: categoryMeals[index].duration,
+          complexity: categoryMeals[index].complexity,
+          affordability: categoryMeals[index].affordability,
+        ),
         itemCount: categoryMeals.length,
       ),
     );
