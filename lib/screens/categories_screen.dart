a6d4a4bf49d9142ca987ca1416515c3f35865821
Long Do <longdo@dobtech.vn>
2020-12-29 20:55:52 +0700

    add tabs as home screen to navigate between category and favorites screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,27 +6,22 @@ import '../dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map(
-              (category) => CategoryItem(
-                title: category.title,
-                color: category.color,
-                id: category.id,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-          childAspectRatio: 3 / 2,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (category) => CategoryItem(
+              title: category.title,
+              color: category.color,
+              id: category.id,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        childAspectRatio: 3 / 2,
       ),
     );
   }
